6abf7fb3a83eb9fc446a92bc8caee5625bf1c3bb
IvanFadieiev <ivanfadeev91@gmail.com>
2015-12-07 23:57:19 +0200

    Image#delete#edit#update

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,20 +9,28 @@ class ImagesController < ActionController::Base
     @torre_travel = TorreTravel.find(params[:torre_travel_id])
     @image = @torre_travel.images.build(image_params)
       if @image.save
-        redirect_to :back #all_types_path
+        redirect_to :back
       else
         render 'new'
       end
   end
 
   def edit
-    
+    @image = Image.find(params[:id])
   end
   def update
-    
+    @image = Image.find(params[:id])
+    if @image.update(image_params)
+       redirect_to torre_travel_path(@image.torre_travel_id), notice: 'Изображение обновлено!!!'
+     else
+       render 'edit'
+     end
   end
   def destroy
-    
+    @image = Image.find(params[:id])
+    if @image.destroy
+      redirect_to :back, notice: 'Изображение удалено!!!'
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :torre_travels do
     resources :images
   end
-  # resources :images, only: [:edit, :update, :destroy]
+  resources :images, only: [:edit, :update, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   get 'all_types',   to: 'torre_travels#all_housings'
   get 'housing',   to: 'torre_travels#housing'
