40c3182e2978b2a92e6e840fe5140f1a6a855149
IvanFadieiev <ivanfadeev91@gmail.com>
2016-01-10 17:29:09 +0200

    comments_fix

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,6 @@ class AdminMailer < ApplicationMailer
 
   def torre_travel_destroyed(house)
     @torre_travel = house
-    # attachments.inline['missing2.png'] = File.read('app/assets/images/small/missing2.png')
     attachments.inline['filename.jpg'] = {mime_type: 'application/x-gzip',
                                     content: @torre_travel.images.image_of_housing.url(:small)}
     mail to: 'torretravel2015@gmail.com',
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
+  default_scope -> { order('created_at DESC') }
   validates :name, presence: true, length: { minimum: 3 }
-  validates :body, presence: true,  length: { in: 1..200 }
+  validates :body, presence: true,  length: { in: 1..160 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }
 end
