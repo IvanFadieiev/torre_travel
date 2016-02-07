66e8ecd0ed6a6c049e6740af16a4ffdd438b1e97
IvanFadieiev <ivanfadeev91@gmail.com>
2016-02-07 20:09:41 +0200

    rus admin

--- app/models/torre_travel.rb
+++ app/models/torre_travel.rb
@@ -1,13 +1,18 @@
 class TorreTravel < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   has_many :images, dependent: :destroy
+
   enum housing: [:house, :flat, :bungalo, :chelet]
   enum distance: [:first_line, :second_line]
   enum reserved: [:reserv, :unreserv]
+
   validates :description, presence: true,
             length: { minimum: 5 }
   validates :description_es, presence: true,
             length: { minimum: 5 }
   validates :price, presence: true
   validates :housing, presence: true
+  def self.restricted_statuses
+    statuses.except :failed, :destroyed
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module ToreTravelProj
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
+    config.i18n.default_locale = :ru
   end
 end
